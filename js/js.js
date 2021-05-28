7bbb75e05b2339dd59f3f9543988f29f2e58d6f5
BeccAYYY <becsell@live.com.au>
2021-05-28 12:13:17 +1000

    Filled first page and made checkbox function in Js

--- js/js.js
+++ js/js.js
@@ -1,9 +1,19 @@
+//Function to set Local Storage "page" to current ID of the radio button selected
 function storePage(e) {
     localStorage.setItem("page", e);
 }
 
+//On page load, set the checked radio button to the one with the ID from Local Storage (if "page" exists in Local Storage)
 if (localStorage.getItem("page")) {
     document.getElementById(localStorage.getItem("page")).checked = true;
 } else {
     document.getElementById("page4").checked = true;
+}
+
+function check(x) {
+    if (x.firstChild.classList.contains("bi-square")) {
+        x.innerHTML = '<svg width="32" height="32" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>';
+    } else {
+        x.innerHTML = '<svg width="32" height="32" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/></svg>';
+    }
 }
